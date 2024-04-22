71e669dd0abf65d5ba8aa912351081b1461cf344
Julio <juliocesarcs2004@gmail.com>
2024-04-22 18:47:52 -0300

    Realizada a refatoracao do contexto Pet

--- src/main/java/br/com/alura/adopet/api/controller/PetController.java
+++ src/main/java/br/com/alura/adopet/api/controller/PetController.java
@@ -1,14 +1,14 @@
 package br.com.alura.adopet.api.controller;
 
+import br.com.alura.adopet.api.exception.ValidacaoException;
 import br.com.alura.adopet.api.model.Pet;
-import br.com.alura.adopet.api.repository.PetRepository;
+import br.com.alura.adopet.api.service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,18 +16,15 @@ import java.util.List;
 public class PetController {
 
     @Autowired
-    private PetRepository repository;
+    PetService petService;
 
     @GetMapping
-    public ResponseEntity<List<Pet>> listarTodosDisponiveis() {
-        List<Pet> pets = repository.findAll();
-        List<Pet> disponiveis = new ArrayList<>();
-        for (Pet pet : pets) {
-            if (pet.getAdotado() == false) {
-                disponiveis.add(pet);
-            }
+    public ResponseEntity<String> listarTodosDisponiveis() {
+        try{
+            List<Pet> disponiveis = petService.listarPetService();
+        } catch (ValidacaoException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
-        return ResponseEntity.ok(disponiveis);
+        return ResponseEntity.ok().build();
     }
-
 }
